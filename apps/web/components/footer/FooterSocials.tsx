ace8a1166af3622aaf6840cbe2c11d903c9bd7fe
codevoks <vanshkumar841@gmail.com>
2026-01-31 23:48:21 +0530

    add a seperate socials.ts file to track socials

--- apps/web/components/footer/FooterSocials.tsx
+++ apps/web/components/footer/FooterSocials.tsx
@@ -1,14 +1,9 @@
-import { FaInstagram, FaXTwitter, FaReddit, FaYoutube } from 'react-icons/fa6'
 import { socials } from './socials/socials.ts'
 export const FooterSocials = () => {
   return (
     <div className="flex">
       <div className="footer-socials">Follow us on -</div>
       <div className="footer-socials-icons">
-        {/* <FaInstagram className="social-icon" />
-        <FaXTwitter className="social-icon" />
-        <FaReddit className="social-icon" />
-        <FaYoutube className="social-icon" /> */}
         {socials.map(({ label, url, icon: Icon }) => (
           <a
             key={label}
