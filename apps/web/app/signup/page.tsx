3112559e5cdb4ccd6d0b1a1090a5e4252cd792eb
codevoks <vanshkumar841@gmail.com>
2026-02-03 22:38:36 +0530

    sign up image added

--- apps/web/app/signup/page.tsx
+++ apps/web/app/signup/page.tsx
@@ -1,9 +1,16 @@
 import { Card } from '../../components/cards/Card'
 import { signUpCardData } from '../../components/cards/cardData'
+import Image from 'next/image'
 
 export default function Signup() {
   return (
-    <div>
+    <div className="flex">
+      <Image
+        src="/images/signup.png"
+        width={1500}
+        height={500}
+        alt="Picture of the author"
+      />
       <Card
         message={signUpCardData.message}
         inputs={signUpCardData.inputs}
