b3cf46b77ad141c4292ceb265105d5de844acc5f
codevoks <vanshkumar841@gmail.com>
2026-02-04 15:03:06 +0530

    added login check for app bar

--- apps/web/components/appbar/Appbar.tsx
+++ apps/web/components/appbar/Appbar.tsx
@@ -1,17 +1,41 @@
 'use client'
+import { useEffect, useState } from 'react'
+import { usePathname } from 'next/navigation'
 import { PrimaryButton } from '../buttons/PrimaryButton'
 import { SecondaryButton } from '../buttons/SecondaryButton'
+import { getRequest } from '../../apiService'
 
 export const Appbar = () => {
+  const [loggedIn, setLoggedIn] = useState<boolean>(false)
+  const pathname = usePathname()
+
+  useEffect(() => {
+    async function checkLoggedIn() {
+      try {
+        const response = await getRequest({ path: '/me', data: {} })
+        if (response.status === 200) {
+          setLoggedIn(true)
+        } else {
+          setLoggedIn(false)
+        }
+      } catch (error) {}
+    }
+    checkLoggedIn()
+  }, [pathname])
   return (
     <div className="appbar">
       <div>
         <PrimaryButton title="Home" path=""></PrimaryButton>
       </div>
       <div className="flex">
-        <SecondaryButton title="Sign Up" path="signup"></SecondaryButton>
-        <SecondaryButton title="log In" path="login"></SecondaryButton>
-        <SecondaryButton title="Log Out" path=""></SecondaryButton>
+        {loggedIn ? (
+          <SecondaryButton title="Log Out" path=""></SecondaryButton>
+        ) : (
+          <div>
+            <SecondaryButton title="Sign Up" path="signup"></SecondaryButton>
+            <SecondaryButton title="Log In" path="login"></SecondaryButton>
+          </div>
+        )}
       </div>
     </div>
   )
