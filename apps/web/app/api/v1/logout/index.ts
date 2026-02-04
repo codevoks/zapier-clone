cd232bbd54fcbf34320442ebee4d92cfd9ec150e
codevoks <vanshkumar841@gmail.com>
2026-02-04 22:46:27 +0530

    api and navigation added to primary and secondary buttons

--- apps/web/app/api/v1/logout/index.ts
+++ apps/web/app/api/v1/logout/index.ts
@@ -1,3 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function POST(Request: NextRequest) {}
+export async function POST(Request: NextRequest) {
+  try {
+  } catch (error) {}
+}
