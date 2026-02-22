28b4f16a95883b13bfb5c7a2332ad8b8d96e0bd5
codevoks <vanshkumar841@gmail.com>
2026-02-22 13:42:09 +0530

    Style zap table

--- apps/web/components/zap/zapsTable/ZapsTable.tsx
+++ apps/web/components/zap/zapsTable/ZapsTable.tsx
@@ -4,15 +4,19 @@ import ZapsBar from './ZapsBar'
 
 export default function ZapTable({ zaps }: { zaps: Zap[] }) {
   return (
-    <table>
-      <thead>
-        <ZapsBar cells={['Name', 'ID', 'Created at', 'Webhook URL', 'Go']} />
-      </thead>
-      <tbody>
-        {zaps.map(zap => (
-          <ZapsBar key={zap.id} zap={zap} />
-        ))}
-      </tbody>
-    </table>
+    <div className="zaps-table-wrap">
+      <div className="overflow-x-auto">
+        <table>
+          <thead>
+            <ZapsBar cells={['Name', 'ID', 'Created at', 'Webhook URL', 'Go']} />
+          </thead>
+          <tbody>
+            {zaps.map(zap => (
+              <ZapsBar key={zap.id} zap={zap} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
