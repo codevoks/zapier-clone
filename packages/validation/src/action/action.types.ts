b8d3461f06df881f0dfb3daf3e2d39aa3cf6010b
codevoks <vanshkumar841@gmail.com>
2026-03-01 14:02:39 +0530

    Fix ActionSolanaSchemaType inference to use the correct schema

--- packages/validation/src/action/action.types.ts
+++ packages/validation/src/action/action.types.ts
@@ -2,4 +2,4 @@ import { z } from 'zod'
 import { ActionEmailSchema, ActionSolanaSchema } from './action.schema'
 
 export type ActionEmailSchemaType = z.infer<typeof ActionEmailSchema>
-export type ActionSolanaSchemaType = z.infer<typeof ActionEmailSchema>
+export type ActionSolanaSchemaType = z.infer<typeof ActionSolanaSchema>
