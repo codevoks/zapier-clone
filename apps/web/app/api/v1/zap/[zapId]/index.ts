eac2f5671ba3d10173cd4689078edac03b317eca
codevoks <vanshkumar841@gmail.com>
2026-03-03 15:53:55 +0530

    Add PUT endpoint for updating Zaps with trigger and action parsing validation

--- apps/web/app/api/v1/zap/[zapId]/index.ts
+++ apps/web/app/api/v1/zap/[zapId]/index.ts
@@ -1,6 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { prisma } from '@repo/db'
 import { verifyJwt } from '@repo/auth'
+import {
+  safeParseZapCreteSchema,
+  safeParseTriggersAndActions,
+} from '@repo/validation'
 
 const JWT_SECRET = 'shallom'
 
@@ -43,6 +47,58 @@ export async function GET(
       return NextResponse.json({ error: 'Zap not found.' }, { status: 404 })
     }
     return NextResponse.json({ zap }, { status: 200 })
+  } catch (error) {
+    return NextResponse.json(
+      { error: 'Error while fetching Zaps' },
+      { status: 500 }
+    )
+  }
+}
+
+export async function PUT(
+  Request: NextRequest,
+  { params }: { params: { zapId: string } }
+) {
+  try {
+    const token = Request.cookies.get('token')?.value
+    if (!token) {
+      return NextResponse.json(
+        { error: 'Login token not found.' },
+        { status: 411 }
+      )
+    }
+    const parsedToken = await verifyJwt(token, JWT_SECRET)
+    if (!parsedToken?.userId) {
+      return NextResponse.json({ error: 'Invalid Token.' }, { status: 411 })
+    }
+    const body = await Request.json()
+    const parsedBody = await safeParseZapCreteSchema(body)
+    if (!parsedBody.success || !safeParseTriggersAndActions(parsedBody.data)) {
+      return NextResponse.json({ error: 'Zap Parsing Failed' }, { status: 411 })
+    }
+    const newZap = await prisma.zap.update({
+      where: {
+        id: params.zapId,
+      },
+      data: {
+        trigger: {
+          update: {
+            triggerId: parsedBody.data.availableTriggerId,
+            metadata: parsedBody.data.triggerMetadata,
+          },
+        },
+        actions: {
+          deleteMany: {},
+          create: parsedBody.data.actions.map((x, index) => ({
+            actionId: x.availableActionId,
+            sortingOrder: index,
+            metadata: x.actionMetadata,
+          })),
+        },
+      },
+    })
+
+    return NextResponse.json({ zap: newZap }, { status: 200 })
   } catch (error) {
     return NextResponse.json(
       { error: 'Error while fetching Zaps' },
