5b5861f596d87a525a35a6d11e57d5637ac33d65
codevoks <vanshkumar841@gmail.com>
2026-03-03 22:44:52 +0530

    Integrate Prisma to fetch zapRun details in Kafka consumer; add error handling for missing zapRunId

--- apps/processor/src/index.ts
+++ apps/processor/src/index.ts
@@ -1,4 +1,5 @@
 import { Kafka } from 'kafkajs'
+import { prisma } from '@repo/db'
 
 const TOPIC_NAME = 'zap-events'
 
@@ -8,28 +9,53 @@ const kafka = new Kafka({
 })
 
 async function main() {
-  const consumer = kafka.consumer({ groupId: 'main-worker' })
-  await consumer.connect()
+  try {
+    const consumer = kafka.consumer({ groupId: 'main-worker' })
+    await consumer.connect()
 
-  await consumer.subscribe({ topic: TOPIC_NAME, fromBeginning: true })
+    await consumer.subscribe({ topic: TOPIC_NAME, fromBeginning: true })
 
-  await consumer.run({
-    autoCommit: false,
-    eachMessage: async ({ topic, partition, message }) => {
-      console.log({
-        partition,
-        offset: message.offset,
-        value: message.value?.toString(),
-      })
-      await consumer.commitOffsets([
-        {
-          topic: TOPIC_NAME,
-          partition: partition,
-          offset: (parseInt(message.offset) + 1).toString(),
-        },
-      ])
-    },
-  })
+    await consumer.run({
+      autoCommit: false,
+      eachMessage: async ({ topic, partition, message }) => {
+        const zapRunId = message.value?.toString()
+        if (!zapRunId) {
+          console.log(' No zapRunId received')
+        }
+        const zapRun = await prisma.zapRun.findUnique({
+          where: { id: zapRunId },
+          include: {
+            zap: {
+              include: {
+                trigger: true,
+                actions: true,
+              },
+            },
+          },
+        })
+        if (!zapRun) {
+          console.log(
+            "I don't know if should return, continue or do what ? Return will stop the consumer all together ?"
+          )
+        }
+        console.log('ZAP=> ' + JSON.stringify(zapRun))
+        console.log({
+          partition,
+          offset: message.offset,
+          value: message.value?.toString(),
+        })
+        await consumer.commitOffsets([
+          {
+            topic: TOPIC_NAME,
+            partition: partition,
+            offset: (parseInt(message.offset) + 1).toString(),
+          },
+        ])
+      },
+    })
 
-  while (true) {}
+    while (true) {}
+  } catch (error) {
+    console.error('Error in processor ' + error)
+  }
 }
