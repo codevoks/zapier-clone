4a6034431ccf1572003bd6798ea2f2f264ff8c4d
codevoks <vanshkumar841@gmail.com>
2026-03-08 17:05:00 +0530

    Add Resend integration for email sending in processor service, update dependencies, and adjust TypeScript configuration

--- packages/processor/src/actions/service/actions.service.ts
+++ packages/processor/src/actions/service/actions.service.ts
@@ -1,3 +1,5 @@
+import { Resend } from 'resend'
+
 export async function sendEmail({
   toEmail,
   subject,
@@ -11,8 +13,28 @@ export async function sendEmail({
     console.log('TO EMAIL ' + toEmail)
     console.log('SUBJECT ' + subject)
     console.log('BODY TEMPLATE ' + bodyTemplate)
+    const apiKey = process.env.RESEND_KEY
+    const fromEmail = process.env.SEND_EMAIL
+    if (apiKey === undefined || fromEmail === undefined) {
+      console.log('No ENV variable defined')
+      return
+    }
+    const resend = new Resend(apiKey)
+    const { data, error } = await resend.emails.send({
+      from: fromEmail,
+      to: [toEmail],
+      subject: subject,
+      text: bodyTemplate,
+      html: '<strong>It works!</strong>',
+    })
+    if (error) {
+      console.log('Error in resend function ' + error)
+      throw error
+    }
+    console.log('Email sent successfully')
   } catch (error) {
     console.log('Error in sending email ' + error)
+    throw error
   }
 }
 
